afaf6bbe5a6584701a355cabefb2dcb1f6db9f6c
nathalhavitoriakkk <106328684+nathalhavitoriakkk@users.noreply.github.com>
2023-10-27 09:36:36 -0300

    js

---
+++ jogo/jogo.js
@@ -0,0 +1,16 @@
+const celulas = document,querySelector(".celula");
+
+let checarTurno = true;
+
+let turno;
+
+const Jogador_X = "X";
+const Jogador_O = "O";
+
+let jogoAcabou = false;
+
+document.addEventListener ("click", (event) =>) {
+    if (event.target.matches (".celula") && !jogoAcabou) {
+Jogar
+    }
+}
